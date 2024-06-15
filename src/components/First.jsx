4df7a5c82ea868693b0a0494176f2bb8688ac378
amarRajputDev <amarrajputvines@gmail.com>
2024-06-15 18:27:14 +0530

    final deploy

--- src/components/First.jsx
+++ src/components/First.jsx
@@ -40,28 +40,28 @@ function First() {
    })
 
   return (
-    <div className= {`w-screen h-screen main flex flex-col sm:items-center p-10`}>
-        <div className='w-[80%] h-[100px] flex  '>
+    <div className= "w-screen h-screen main flex flex-col sm:items-center sm:p-10">
+        <div className='w-[80%] h-[100px] flex pl-10 sm:pt-0 pt-7'>
             <img src={logo} alt="" className=' w-8 h-6' />
             <h1 className='font-bold'>Westmire Wired</h1>
         </div>
         {/* container */}
-        <div className='flex  flex-col-reverse items-center sm:flex-row'>
+        <div className='flex  flex-col-reverse items-center sm:h-[70%] sm:items-center sm:justify-center sm:flex-row'>
         {/* //left */}
-            <div className='w-[40%]  flex flex-col justify-center items-center ml-[10%]'> 
+            <div className='sm:w-[40%] w-[100%] sm:h-[100%] flex flex-col justify-center items-center '> 
                 <div>
-                    <h1 className=' sm:text-8xl text-[270%] ' ref={text}>The&nbsp;Westmire</h1><br />
+                    <h1 className=' sm:text-8xl text-[270%]' ref={text}>The&nbsp;Westmire</h1>
                     <h1 className=' sm:text-8xl text-[270%] font-bold' ref={text2}>A56 Headset</h1>
                 </div>
-                <div className="buttons flex gap-4 p-10">
+                <div className="buttons flex gap-4 sm:p-10 mt-10 sm:mt-0">
                     <button className='bg-white w-[9rem] rounded-3xl h-11'>Buy Now</button>
                     <button className=' bg-slate-200 hover:bg-white w-[9rem] rounded-3xl'>Shop All</button>
                 </div>
                
             </div>
             {/* right  */}
-            <div className='w-[60%]'>
-            <img className='head' src={Headphones} alt=""  />
+            <div className='w-[60%] sm:w-[40%]  '>
+            <img className='head sm:h-[60%] ' src={Headphones} alt=""  />
 
             </div>
         </div>
